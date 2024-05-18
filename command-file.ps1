6e7d227fe369e162e597b913b7fd800a1985ef04
Pasupathikumar S <pasupathi.kumar@c2stechs.com>
2024-05-18 05:39:58 +0530

    commit

---
+++ command-file.ps1
@@ -0,0 +1,5 @@
+az group create --name arm-template --location 'southindia'
+az deployment group create --resource-group arm-template --template-file azure-kubernetes-cluster.json
+az deployment group create --resource-group arm-template --template-file azure-single-vm.json
+az deployment group create --resource-group arm-template --template-file azure-web-app.json
+az deployment group create --resource-group arm-template --template-file azure-storage-account.json
